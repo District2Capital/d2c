d2ef71e0bc28dd6fccf3579b57b70d3f6d3b2ce2
Andreas Bigger <abigger87@gmail.com>
2019-05-12 21:58:48 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# District 2 Capital
-
-## Available Scripts
-
-### `npm start`
-
-Local Development
---
+++ precache-manifest.d60abe28b25f67f4689e68964f589198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dff410365647b117ef36bb9f19d673d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7a6bdc7167a53100316",
+    "url": "/static/css/main.769ee8d9.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a7a6bdc7167a53100316",
+    "url": "/static/js/main.04fcc2b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dc79100c19feae6a2fe85ac950ed322c",
+    "url": "/static/media/logo.dc79100c.png"
+  }
+]);
